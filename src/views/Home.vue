9e551490ef53a6c48bac7d39bd6b6ab6c0bd1699
Jhou-wei-jyun <lf217189@gmail.com>
2021-09-08 17:38:21 +0800

    git user

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" class="m-auto bg-pink-800">
+    <img alt="Vue logo" src="../assets/logo.png" class="m-auto bg-pink-900">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
